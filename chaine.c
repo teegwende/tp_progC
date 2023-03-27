d2cf34991148a6a2c56a87cd9d3dc2b03ffec21c
teegwende <teegwend@gmail.com>
2023-03-27 20:47:40 +0000

    modif fichier chaine.c
    ajout de code de vérification

--- chaine.c
+++ chaine.c
@@ -5,6 +5,9 @@ int main()
   char jours[7][10]={"lun\n","mardi 2","mercredi 3","jeudi 4","vendredi 5","samedi 6","dimanche 7"};
 int score = 0;
   int i;
+  printf("Test affichage jour\n");
+  printf("%s\n",jours[1]);
+
   for (i=0;i<7;i++)
   {
     if(jours[i][0]=='m')
